66a7547e756fdae5715ab9c699df3d822e05fedc
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2016-09-01 21:54:35 +0700

    remove stuff we don't need
    - gravatar
    - toJSON()

--- api/models/User.js
+++ api/models/User.js
@@ -20,27 +20,7 @@ module.exports = {
     passports: {
       collection: 'Passport',
       via: 'user'
-    },
-
-    getGravatarUrl: function () {
-      var md5 = crypto.createHash('md5');
-      md5.update(this.email || '');
-      return 'https://gravatar.com/avatar/'+ md5.digest('hex');
-    },
-
-    toJSON: function () {
-      var user = this.toObject();
-      delete user.password;
-      user.gravatarUrl = this.getGravatarUrl();
-      return user;
     }
-  },
-
-  beforeCreate: function (user, next) {
-    if (_.isEmpty(user.username)) {
-      user.username = user.email;
-    }
-    next();
   },
 
   /**
